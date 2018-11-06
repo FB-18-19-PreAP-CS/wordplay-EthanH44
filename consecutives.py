523e8a86b8811b461ac784b339df0e0a22e9dc02
HUGHES.ETHAN <2210321@fbisd.flourbluffschools.net>
2018-11-06 13:41:14 -0600

    got the words

---
+++ consecutives.py
@@ -0,0 +1,14 @@
+def consecutives():
+    with open("words.txt") as file:
+        for line in file:
+            for word in line.strip().split():
+                for i in range(len(word)-5):
+                    if word[i] == word[i+1] and word[i+2] == word[i+3] and word[i+4] == word[i+5]:
+                        print(word)
+                
+           
+def main():
+    consecutives()
+           
+if __name__ == "__main__":
+    main()
